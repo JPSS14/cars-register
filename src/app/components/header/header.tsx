1290b3e9bcd473e32b7c9caacf4676147535c19c
JPSS14 <jp_scarabelli@hotmail.com>
2024-01-20 17:02:11 -0300

    Add Header component

---
+++ src/app/components/header/header.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import styles from "./header.module.scss";
+
+export const Header = () => {
+  return (
+    <header className={styles.header}>
+      <Image
+        src="cars-register-logo.svg"
+        alt="Cars Register Logo"
+        width={239}
+        height={48}
+        priority
+      />
+    </header>
+  );
+};
