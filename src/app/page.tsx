69358d93fa81678fddec7370b8df1db0f6f2a54f
JPSS14 <jp_scarabelli@hotmail.com>
2024-01-20 19:58:52 -0300

    Add Banner in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { Banner } from "./components";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Banner />
       <div className={styles.teste}>Teste</div>
     </main>
   );
