17363c46d84a55878277dacc405d3115a95bfe6a
JPSS14 <jp_scarabelli@hotmail.com>
2024-01-23 11:16:10 -0300

    Add CardList at Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import { TopSearch } from "./components";
+import { CardList, TopSearch } from "./components";
+import { Cars } from "./constants/cars";
 import styles from "./page.module.scss";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <TopSearch />
+      <CardList items={Cars} />
     </main>
   );
 }
