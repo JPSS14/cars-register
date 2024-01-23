28c7e626187c55bbe2383797754cf934003d5887
JPSS14 <jp_scarabelli@hotmail.com>
2024-01-23 16:04:09 -0300

    Add automakers constant

---
+++ src/app/constants/automakers.ts
@@ -0,0 +1,12 @@
+export interface Automakers {
+  id: number;
+  name: string;
+}
+
+export const automakers: Automakers[] = [
+  { id: 1, name: "volkswagen" },
+  { id: 2, name: "ferrari" },
+  { id: 3, name: "porsche" },
+  { id: 4, name: "mclaren" },
+  { id: 5, name: "lamborghini" },
+];
