eebf34d3f97e5ee37f333d1dba0f185b0f622eec
JPSS14 <jp_scarabelli@hotmail.com>
2024-01-27 22:40:35 -0300

    Add forwardRef in Input component

--- src/app/components/input/input.tsx
+++ src/app/components/input/input.tsx
@@ -1,18 +1,29 @@
+import React from "react";
 import styles from "./input.module.scss";
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string;
 }
 
-export const Input = ({ label, name, ...props }: InputProps) => {
-  return (
-    <>
-      {label && (
-        <label htmlFor={name} className={styles.label}>
-          {label}
-        </label>
-      )}
-      <input {...props} name={name} className={styles.input} />
-    </>
-  );
-};
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ label, name, value, ...props }, ref) => {
+    return (
+      <>
+        {label && (
+          <label htmlFor={name} className={styles.label}>
+            {label}
+          </label>
+        )}
+        <input
+          {...props}
+          name={name}
+          ref={ref}
+          value={value || ""}
+          className={styles.input}
+        />
+      </>
+    );
+  }
+);
+
+Input.displayName = "Input";
