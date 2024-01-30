95cdb47741eded0c9c88a8279469e36782daddb7
JPSS14 <jp_scarabelli@hotmail.com>
2024-01-30 20:28:42 -0300

    Add forwardRef in Select component

--- src/app/components/select/select.tsx
+++ src/app/components/select/select.tsx
@@ -1,26 +1,31 @@
 import { Automakers } from "@/app/constants/automakers";
 import styles from "./select.module.scss";
+import React from "react";
 
 interface SelectProps extends React.SelectHTMLAttributes<HTMLSelectElement> {
   label?: string;
   items: Automakers[];
 }
 
-export const Select = ({ label, name, items, ...props }: SelectProps) => {
-  return (
-    <>
-      {label && (
-        <label htmlFor={name} className={styles.label}>
-          {label}
-        </label>
-      )}
-      <select className={styles.select} {...props}>
-        {items.map((item) => (
-          <option key={item.id} className={styles.option}>
-            {item.name}
-          </option>
-        ))}
-      </select>
-    </>
-  );
-};
+export const Select = React.forwardRef<HTMLSelectElement, SelectProps>(
+  ({ label, name, items, ...props }, _ref) => {
+    return (
+      <>
+        {label && (
+          <label htmlFor={name} className={styles.label}>
+            {label}
+          </label>
+        )}
+        <select className={styles.select} {...props}>
+          {items.map((item) => (
+            <option key={item.id} className={styles.option}>
+              {item.name}
+            </option>
+          ))}
+        </select>
+      </>
+    );
+  }
+);
+
+Select.displayName = "Select";
