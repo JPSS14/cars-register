c0672559e8e181be1b5bf736969c2555345ba4a7
JPSS14 <jp_scarabelli@hotmail.com>
2024-01-31 11:33:52 -0300

    Add Hook Forms in Search component

--- src/app/components/search/search.tsx
+++ src/app/components/search/search.tsx
@@ -1,14 +1,34 @@
+"use client";
+import { useForm, SubmitHandler } from "react-hook-form";
 import { Button, Hr, Input } from "..";
 import styles from "./search.module.scss";
+import { useRegisterContext } from "@/app/contexts/register-context";
+
+interface SearchCar {
+  carName: string;
+}
 
 export const Search = () => {
+  const { setOpenRegisterModal } = useRegisterContext();
+  const { register, handleSubmit } = useForm<SearchCar>();
+
+  const onSubmit: SubmitHandler<SearchCar> = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className={styles.search}>
       <h1>Encontre seu carro</h1>
-      <Input placeholder="Modelo ou montadora" />
-      <Button color="secondary">Buscar</Button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input {...register("carName")} placeholder="Modelo ou montadora" />
+        <Button type="submit" color="secondary">
+          Buscar
+        </Button>
+      </form>
       <Hr />
-      <Button>Registrar Carro</Button>
+      <Button onClick={() => setOpenRegisterModal(true)}>
+        Registrar Carro
+      </Button>
     </div>
   );
 };
