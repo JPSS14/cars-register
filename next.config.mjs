dbebe743cd9498a45e332068f90eb5be02df8d96
JPSS14 <jp_scarabelli@hotmail.com>
2024-02-02 22:31:52 -0300

    Add acepted image url in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+import { hostname } from "os";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "cdn.motor1.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
