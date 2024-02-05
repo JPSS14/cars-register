acae1944b8fa45997823c0526a76db2fe6d8cfa1
JPSS14 <jp_scarabelli@hotmail.com>
2024-02-04 22:01:28 -0300

    Add Hook Forms in RegisterModal component

--- src/app/components/register-modal/register-modal.tsx
+++ src/app/components/register-modal/register-modal.tsx
@@ -1,70 +1,117 @@
 "use client";
 import { automakers } from "@/app/constants/automakers";
-import { Button, Input, Modal, Select, Stepper, TextArea } from "..";
+import { Button, Modal, Select, Stepper, TextArea, Input } from "..";
 import { useRegisterContext } from "@/app/contexts/register-context";
 import styles from "./register-modal.module.scss";
+import { Cars } from "@/app/constants/cars";
+import { useForm, SubmitHandler, Controller } from "react-hook-form";
+import { useRef } from "react";
 
 interface RegisterModalProps {
   isOpen: boolean;
 }
 
 export const RegisterModal = ({ isOpen }: RegisterModalProps) => {
-  const { setStage, stage, setOpenRegisterModal } = useRegisterContext();
+  const { setStage, stage, setOpenRegisterModal, setCarList, carList } =
+    useRegisterContext();
+  const {
+    control,
+    handleSubmit,
+    setValue,
+    register,
+    formState: { errors },
+  } = useForm<Cars>();
+
+  const onSubmit: SubmitHandler<Cars> = (data) => {
+    setOpenRegisterModal(false);
+    setCarList([...carList, data]);
+    console.log(data);
+  };
+
+  const inputRef = useRef(null);
+
   return (
     <Modal isOpen={isOpen}>
       <Stepper />
-      {stage === "initial" && (
-        <div className={styles.inputsSection}>
-          <div>
-            <Select items={automakers} label="Montadora" />
-            <Input label="Modelo" placeholder="Digite o nome do modelo" />
-            <Input label="Ano" placeholder="Digite o ano do carro" />
-            <Input label="Imagem" placeholder="Selecione uma imagem 1920px" />
+      <form onSubmit={handleSubmit(onSubmit)} style={{ height: "100%" }}>
+        {stage === "initial" && (
+          <div className={styles.inputsSection}>
+            <div>
+              <Controller
+                name="automaker"
+                control={control}
+                render={({ field }) => (
+                  <Select {...field} items={automakers} label="Montadora" />
+                )}
+              />
+              <Input
+                {...register("model")}
+                label="Modelo"
+                placeholder="Digite o nome do modelo"
+              />
+              <Input
+                {...register("year")}
+                label="Ano"
+                placeholder="Digite o ano do carro"
+              />
+              <Input
+                {...register("image")}
+                label="Imagem"
+                placeholder="Selecione uma imagem 1920px"
+              />
+            </div>
+            <div className={styles.footer}>
+              <Button onClick={() => setStage("status")}>Próximo</Button>
+            </div>
           </div>
-          <div className={styles.footer}>
-            <Button onClick={() => setStage("status")}>Próximo</Button>
-          </div>
-        </div>
-      )}
+        )}
 
-      {stage === "status" && (
-        <div className={styles.inputsSection}>
-          <div>
-            <Input
-              label="Potência"
-              placeholder="Digite a potência do carro (cv)"
-            />
-            <Input
-              label="Torque"
-              placeholder="Digite o torque do carro (kgfm)"
-            />
-            <Input
-              label="Velocidade Máxima"
-              placeholder="Digite a velocidade máxima"
-            />
+        {stage === "status" && (
+          <div className={styles.inputsSection}>
+            <div>
+              <Input
+                {...register("power")}
+                label="Potência"
+                placeholder="Digite a potência do carro (cv)"
+              />
+              <Input
+                {...register("torque")}
+                label="Torque"
+                placeholder="Digite o torque do carro (kgfm)"
+              />
+              <Input
+                {...register("maximumSpeed")}
+                label="Velocidade Máxima"
+                placeholder="Digite a velocidade máxima"
+              />
+            </div>
+            <div className={styles.footer}>
+              <Button onClick={() => setStage("description")}>Próximo</Button>
+            </div>
           </div>
-          <div className={styles.footer}>
-            <Button onClick={() => setStage("description")}>Próximo</Button>
-          </div>
-        </div>
-      )}
+        )}
 
-      {stage === "description" && (
-        <div className={styles.inputsSection}>
-          <div>
-            <Input label="Título" placeholder="Digite um título" />
-            <TextArea
-              label="Velocidade Máxima"
-              placeholder="Digite a velocidade máxima"
-            />
+        {stage === "description" && (
+          <div className={styles.inputsSection}>
+            <div>
+              <Input
+                {...register("description")}
+                label="Título"
+                placeholder="Digite um título"
+              />
+
+              <TextArea
+                label="Velocidade Máxima"
+                placeholder="Digite a velocidade máxima"
+                {...register("description")}
+              />
+            </div>
+            <div className={styles.footer}>
+              <Button type="submit">Finalizar</Button>
+            </div>
           </div>
-          <div className={styles.footer}>
-            <Button onClick={() => setOpenRegisterModal(false)}>
-              Finalizar
-            </Button>
-          </div>
-        </div>
-      )}
+        )}
+      </form>
     </Modal>
   );
 };
