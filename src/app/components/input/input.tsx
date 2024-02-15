34d5993358629e7c24febe517839250205bfe5b9
JPSS14 <jp_scarabelli@hotmail.com>
2024-02-15 16:47:00 -0300

    Add errorMessage in Input

--- src/app/components/input/input.tsx
+++ src/app/components/input/input.tsx
@@ -3,19 +3,28 @@ import styles from "./input.module.scss";
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string;
+  errorMessage?: string;
 }
 
 export const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ label, name, ...props }, ref) => {
+  ({ label, errorMessage, name, ...props }, ref) => {
     return (
-      <>
+      <div className={styles.inputContainer}>
         {label && (
           <label htmlFor={name} className={styles.label}>
             {label}
           </label>
         )}
-        <input {...props} name={name} ref={ref} className={styles.input} />
-      </>
+        <input
+          {...props}
+          name={name}
+          ref={ref}
+          className={`${styles.input} ${errorMessage && styles.errorInput}`}
+        />
+        {errorMessage && (
+          <span className={styles.errorMessage}>{errorMessage}</span>
+        )}
+      </div>
     );
   }
 );
