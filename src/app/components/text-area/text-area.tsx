79ca3babf73651f06b2e990347ea0bc951056cc8
JPSS14 <jp_scarabelli@hotmail.com>
2024-02-16 14:53:00 -0300

    Add errorMessage in TextArea component

--- src/app/components/text-area/text-area.tsx
+++ src/app/components/text-area/text-area.tsx
@@ -4,20 +4,31 @@ import styles from "./text-area.module.scss";
 interface TextAreaProps
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
   label?: string;
+  errorMessage?: string;
 }
 
 export const TextArea = React.forwardRef<HTMLTextAreaElement, TextAreaProps>(
-  ({ label, children, name, ...props }, ref) => {
+  ({ label, errorMessage, children, name, ...props }, ref) => {
     return (
-      <div className={styles.container}>
+      <div className={styles.textAreaContainer}>
         {label && (
           <label htmlFor={name} className={styles.label}>
             {label}
           </label>
         )}
-        <textarea {...props} name={name} className={styles.textArea} ref={ref}>
+        <textarea
+          {...props}
+          name={name}
+          className={`${styles.textArea} ${
+            errorMessage && styles.errorTextArea
+          }`}
+          ref={ref}
+        >
           {children}
         </textarea>
+        {errorMessage && (
+          <span className={styles.errorMessage}>{errorMessage}</span>
+        )}
       </div>
     );
   }
