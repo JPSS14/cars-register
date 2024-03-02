4e3bd4c0a26e6cd19565432d61e96e9293dfcb7e
JPSS14 <jp_scarabelli@hotmail.com>
2024-03-01 23:14:17 -0300

    Add Link in Header

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -1,16 +1,19 @@
 import Image from "next/image";
 import styles from "./header.module.scss";
+import Link from "next/link";
 
 export const Header = () => {
   return (
     <header className={styles.header}>
-      <Image
-        src="/cars-register-logo.svg"
-        alt="Cars Register Logo"
-        width={239}
-        height={48}
-        priority
-      />
+      <Link href="/">
+        <Image
+          src="/cars-register-logo.svg"
+          alt="Cars Register Logo"
+          width={239}
+          height={48}
+          priority
+        />
+      </Link>
     </header>
   );
 };
