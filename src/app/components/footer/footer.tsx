00056eabae31cee446367e1a9ebf4a546a8b1176
JPSS14 <jp_scarabelli@hotmail.com>
2024-03-02 22:16:07 -0300

    Add Link in Footer component

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -1,16 +1,19 @@
 import Image from "next/image";
 import styles from "./footer.module.scss";
+import Link from "next/link";
 
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <Image
-        src="/cars-register-logo.svg"
-        alt="Cars Register Logo"
-        width={239}
-        height={48}
-        priority
-      />
+      <Link href="/">
+        <Image
+          src="/cars-register-logo.svg"
+          alt="Cars Register Logo"
+          width={239}
+          height={48}
+          priority
+        />
+      </Link>
     </footer>
   );
 };
