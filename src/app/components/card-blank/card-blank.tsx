6b832a7cab58c571dcf1b061dca7a8a56afe998e
JPSS14 <jp_scarabelli@hotmail.com>
2024-03-07 23:04:16 -0300

    Add active register modal behavior in CardBlank

--- src/app/components/card-blank/card-blank.tsx
+++ src/app/components/card-blank/card-blank.tsx
@@ -1,10 +1,15 @@
 import Image from "next/image";
 import styles from "./card-blank.module.scss";
 import { IoMdAddCircleOutline } from "react-icons/io";
+import { useRegisterContext } from "@/app/contexts/register-context";
 
 export const CardBlank = () => {
+  const { setOpenRegisterModal } = useRegisterContext();
   return (
-    <div className={styles.cardBlank}>
+    <div
+      className={styles.cardBlank}
+      onClick={() => setOpenRegisterModal(true)}
+    >
       <div className={styles.contentContainer}>
         <Image
           src="/cars-register-mini-logo.svg"
