3ea7af86b7d2c0d88c2359fafe6a8a378b5c72ba
JPSS14 <jp_scarabelli@hotmail.com>
2024-03-11 15:42:31 -0300

    Add style in car page

--- src/app/car/[carId]/page.tsx
+++ src/app/car/[carId]/page.tsx
@@ -1,8 +1,10 @@
+import styles from "./page.module.scss";
+
 export default function Car({ params }: { params: { carId: string } }) {
   return (
-    <>
+    <main className={styles.main}>
       <div>CarId: {params.carId}</div>
       <div>e</div>
-    </>
+    </main>
   );
 }
