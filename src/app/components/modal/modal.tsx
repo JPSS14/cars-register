e85e94b90c56b27ffd414e8e733dfb6aa3dfa4a8
JPSS14 <jp_scarabelli@hotmail.com>
2024-03-20 11:56:09 -0300

    Add dynamic width and height in Modal component

--- src/app/components/modal/modal.tsx
+++ src/app/components/modal/modal.tsx
@@ -4,14 +4,21 @@ import styles from "./modal.module.scss";
 interface ModalProps {
   isOpen: boolean;
   children?: ReactNode;
+  height?: number;
+  width?: number;
 }
 
-export const Modal = ({ isOpen, children }: ModalProps) => {
+export const Modal = ({ isOpen, children, height, width }: ModalProps) => {
   if (!isOpen) return null;
 
   return (
     <div className={styles.modalOverlay}>
-      <div className={styles.modalContainer}>{children}</div>
+      <div
+        className={styles.modalContainer}
+        style={{ height: `${height}px`, width: `${width}px` }}
+      >
+        {children}
+      </div>
     </div>
   );
 };
