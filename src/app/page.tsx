551d37f00c74d95938cda444bb8cd2e6a7c13e28
JPSS14 <jp_scarabelli@hotmail.com>
2024-03-20 12:08:47 -0300

    Add ResetList component in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 import { useEffect } from "react";
-import { CardList, RegisterModal, TopSearch } from "./components";
+import { CardList, RegisterModal, ResetList, TopSearch } from "./components";
 import { useRegisterContext } from "./contexts/register-context";
 import styles from "./page.module.scss";
 import { getLocalStorage } from "./utils/utils";
 
 export default function Home() {
-  const { openRegisterModal, carListFiltered, setCarList } =
+  const { openRegisterModal, carListFiltered, setCarList, carList } =
     useRegisterContext();
 
   useEffect(() => {
@@ -22,6 +22,7 @@ export default function Home() {
       <TopSearch />
       <CardList items={carListFiltered} />
       <RegisterModal isOpen={openRegisterModal} />
+      {carList.length > 4 && <ResetList />}
     </main>
   );
 }
