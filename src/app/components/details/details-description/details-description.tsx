0219306c59e3f039f225c8e8b5b5979fc6df437e
JPSS14 <jp_scarabelli@hotmail.com>
2024-03-26 22:45:58 -0300

    Add RegisterDescriptionModal in DetailsDescription

--- src/app/components/details/details-description/details-description.tsx
+++ src/app/components/details/details-description/details-description.tsx
@@ -3,50 +3,53 @@ import style from "./details-description.module.scss";
 import { IoMdAddCircleOutline } from "react-icons/io";
 import { Cars } from "@/app/constants/cars";
 import { DescriptionSection } from "./description-section";
+import { RegisterDescriptionModal } from "../..";
+import { useRegisterContext } from "@/app/contexts/register-context";
+import { hasDescription } from "@/app/utils/utils";
 
 interface DetailsDescriptionProps {
   carItem: Cars;
 }
 
 export const DetailsDescription = ({ carItem }: DetailsDescriptionProps) => {
-  const handleHasDescription = () => {
-    if (carItem.description) {
-      if (
-        carItem.description.length === 1 &&
-        carItem.description[0].textDescription === "" &&
-        carItem.description[0].title === ""
-      ) {
-        return false;
-      } else {
-        return true;
-      }
-    }
-    return true;
+  const { openRegisterModal, setStage, setOpenRegisterDescriptionModal } =
+    useRegisterContext();
+
+  const handleOpenAddDescriptionModal = () => {
+    setStage("description");
+    setOpenRegisterDescriptionModal(true);
   };
 
-  const isDescription = handleHasDescription();
+  const isDescription = hasDescription(carItem);
   return (
-    <div className={style.detailsDescriptionContainer}>
-      {isDescription && (
-        <article className={style.descriptionArticle}>
-          <header className={style.descriptionArticleHeader}>
-            <h1>Descrição</h1>
-          </header>
-          <div className={style.descriptionSectionListContainer}>
-            <DescriptionSection description={carItem.description} />
+    <>
+      <RegisterDescriptionModal
+        isOpen={openRegisterModal}
+        currentCar={carItem}
+      />
+      <div className={style.detailsDescriptionContainer}>
+        {isDescription && (
+          <article className={style.descriptionArticle}>
+            <header className={style.descriptionArticleHeader}>
+              <h1>Descrição</h1>
+            </header>
+            <div className={style.descriptionSectionListContainer}>
+              <DescriptionSection description={carItem.description} />
+            </div>
+          </article>
+        )}
+        <div
+          className={`${style.withoutDescriptionContainer} ${
+            isDescription && style.withDescription
+          }`}
+          onClick={() => handleOpenAddDescriptionModal()}
+        >
+          <div className={style.withoutDescriptionText}>
+            <IoMdAddCircleOutline className={style.withoutDescriptionIcon} />
+            Adicionar descrição
           </div>
-        </article>
-      )}
-      <div
-        className={`${style.withoutDescriptionContainer} ${
-          isDescription && style.withDescription
-        }`}
-      >
-        <div className={style.withoutDescriptionText}>
-          <IoMdAddCircleOutline className={style.withoutDescriptionIcon} />
-          Adicionar descrição
         </div>
       </div>
-    </div>
+    </>
   );
 };
