8f55c795c1431b5d3778854716cdb0cbdb923f82
JPSS14 <jp_scarabelli@hotmail.com>
2024-09-01 23:16:37 -0300

    test: add powerRating utils test

--- src/app/utils/utils.test.tsx
+++ src/app/utils/utils.test.tsx
@@ -1,7 +1,7 @@
 import "@testing-library/jest-dom";
-import { torqueRating } from "./utils";
+import { powerRating, torqueRating } from "./utils";
 
-describe("a utils test", () => {
+describe("a utils torqueRating test", () => {
   it("a torqueRating test 100", () => {
     expect(torqueRating(100)).toBe(5);
   });
@@ -20,5 +20,27 @@ describe("a utils test", () => {
 
   it("a torqueRating test 10", () => {
     expect(torqueRating(10)).toBe(1);
+  });
+});
+
+describe("a utils powerRating test", () => {
+  it("a powerRating 591", () => {
+    expect(powerRating(591)).toBe(5);
+  });
+
+  it("a powerRating 589", () => {
+    expect(powerRating(589)).toBe(4);
+  });
+
+  it("a powerRating 479", () => {
+    expect(powerRating(479)).toBe(3);
+  });
+
+  it("a powerRating 104", () => {
+    expect(powerRating(104)).toBe(2);
+  });
+
+  it("a powerRating 84", () => {
+    expect(powerRating(84)).toBe(1);
   });
 });
